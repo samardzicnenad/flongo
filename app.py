6fb0d4450c919c85129ff827e45a0f9465d4a449
Nenad Samardzic <samardzicnenad@gmail.com>
2016-03-01 17:47:01 -0500

    Added and linked two additional pages

--- app.py
+++ app.py
@@ -11,5 +11,13 @@ db = client.flongodb
 def index():
     return render_template('index.html')
 
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
