cdec59f9fc580b4459d6f615d490b28319d4d992
Nenad Samardzic <samardzicnenad@gmail.com>
2016-03-30 00:24:50 -0400

    Some more changes related to templates

--- flongo/views.py
+++ flongo/views.py
@@ -2,7 +2,7 @@ from flongo import app
 from flask import render_template, request
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def welcome():
     return render_template('welcome.html')
 
@@ -10,7 +10,7 @@ def welcome():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        return render_template('dummy.html')
+        pass
     else:
         return render_template('signup.html')
 
@@ -18,9 +18,25 @@ def signup():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        return render_template('dummy.html')
+        pass
     else:
         return render_template('login.html')
+
+
+@app.route('/dummy', methods=['GET', 'POST'])
+def dummy():
+    if request.method == 'GET':
+        pass
+    else:
+        return render_template('dummy.html')
+
+
+@app.route('/index', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        pass
+    else:
+        return render_template('index.html')
 
 
 @app.route('/error', methods=['GET', 'POST'])
