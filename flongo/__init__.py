9e2f04f63e00bb4e840cd76bb9b7f0e4253ffc0d
Nenad Samardzic <samardzicnenad@gmail.com>
2016-03-31 00:30:48 -0400

    Introduced libsass

--- flongo/__init__.py
+++ flongo/__init__.py
@@ -1,6 +1,7 @@
 import os
 import flask
 from pymongo import MongoClient
+from sassutils.wsgi import SassMiddleware
 
 app = flask.Flask(__name__)
 
@@ -8,3 +9,7 @@ import flongo.views
 
 mongo_client = MongoClient(os.environ['MONGO_PORT_27017_TCP_ADDR'], 27017)
 db = mongo_client.flongodb
+
+app.wsgi_app = SassMiddleware(app.wsgi_app, {
+    'flongo': ('static/sass', 'static/css', '/static/css')
+})
